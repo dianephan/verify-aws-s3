5a30dbeaf8bbdb0a9fe36a95e9b5a82a366df27c
dIANE phan <dianephan12@gmail.com>
2021-07-13 16:13:49 -0700

    update templates folder to make it relevant to this proj

---
+++ settings.py
@@ -0,0 +1,6 @@
+# this is the hardcoded database of people 
+KNOWN_PARTICIPANTS = {
+  'twilioisthebest@twilio.com': '<YOUR_PHONE_NUMBER>',
+  'cedric@twilioquestgame.com': '+15552211986',
+  'twilioquestthebest@twilio.com': '+15553242003'
+}
